d101cf4758099b84b0da8487533013f954ffc472
gutoDePaula <gutoramosdepaula@gmail.com>
2023-01-12 11:52:23 -0300

    Criando cadastro turma. #2

---
+++ cadastroDaTurma.js
@@ -0,0 +1,14 @@
+//associa os elementos html com variáveis JavaScript
+
+let   entradaAno = document.getElementById('ano-classe')
+let   entradaTurma = document.getElementById('turma-classe')
+let   entradaNomeDoAluno = document.getElementById('nome-do-aluno')
+let   entradaRA = document.getElementById('RA')
+let   entradaEmail = document.getElementById('e-mail')
+let   entradaAnotações = document.getElementById('anotações')
+
+//pegar dados e mostrar como teste
+function adicionarAluno(){
+    
+}
+
