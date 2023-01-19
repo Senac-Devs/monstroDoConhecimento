e1765a24ba22e69ed946cafd7c931b82e40a1bf0
gutoDePaula <gutoramosdepaula@gmail.com>
2023-01-19 11:58:39 -0300

    Exibição de alunos parcialmente feita. #9

--- bancodedados.js
+++ bancodedados.js
@@ -7,7 +7,70 @@ bancoDados.transaction(function (criar) {
 function salvarAluno(aluno) {
     bancoDados.transaction(function (inserir) {
         inserir.executeSql("INSERT INTO alunos (infos) VALUES (?)", [
-            JSON.stringify(aluno)
+            JSON.stringify(aluno),
         ]);
+    });
+    //validacao?
+    document.getElementById("ano-classe").value = "";
+    document.getElementById("turma-classe").value = "";
+    document.getElementById("nome-do-aluno").value = "";
+    document.getElementById("RA").value = "";
+    document.getElementById("e-mail").value = "";
+    document.getElementById("anotacoes").value = "";
+}
+
+function exibirTurma() {
+    const [ano, turma] = document.getElementById('turmas-lista').value.split("º - ")
+    console.log(ano, turma)
+    carregaAnoTurma();
+    bancoDados.transaction(function (exibe) {
+        exibe.executeSql(
+            "SELECT * FROM alunos",
+            [],
+            function (exibe, resultados) {
+                document.getElementById("turmas").innerHTML = "";
+                const tamanho = resultados.rows.length;
+                let item;
+
+                for (let i = 0; i < tamanho; i++) {
+                    item = resultados.rows.item(i);
+                    const aluno = JSON.parse(item.infos);
+                    if (aluno.ano == ano && aluno.turma == turma){
+                        exibeAlunoHMTL(aluno)
+                    }
+                }
+            }
+        );
+    });
+}
+
+function exibeAlunoHMTL(aluno){
+    document.getElementById('turmas').innerHTML += `<p>${aluno.nome}</p>`
+}
+
+
+let anoTurmaInserir = [];
+let alunos;
+function carregaAnoTurma() {
+    bancoDados.transaction(function (exibe) {
+        exibe.executeSql(
+            "SELECT * FROM alunos",
+            [],
+            function (exibe, resultados) {
+                document.getElementById("turmas").innerHTML = "";
+                const tamanho = resultados.rows.length;
+                let item;
+                alunos = [];
+                for (let i = 0; i < tamanho; i++) {
+                    item = JSON.parse(resultados.rows.item(i).infos);
+                    alunos.push(item)
+                    const anoTurma = `${item.ano}º - ${item.turma}`;
+                    if (!anoTurmaInserir.includes(anoTurma)) {
+                        anoTurmaInserir.push(anoTurma);
+                        addTurma(anoTurma);
+                    }
+                }
+            }
+        );
     });
 }
--- cadastroDaTurma.js
+++ cadastroDaTurma.js
@@ -6,6 +6,7 @@ let   entradaNomeDoAluno = document.getElementById('nome-do-aluno')
 let   entradaRA = document.getElementById('RA')
 let   entradaEmail = document.getElementById('e-mail')
 let   entradaAnotacoes = document.getElementById('anotacoes')
+let   retorno = document.getElementById('retorno')
 
 
 /*criar um eventlistener para quando clicar ou mudar para o campo RA
@@ -23,15 +24,25 @@ function adicionarAluno(){
     const aluno = new Aluno (nomeDoAluno, geraRA(), emailAluno, anoClasse, turmaClasse, anotacoesAluno)
     salvarAluno(aluno)
     console.log(aluno)
-
-}
-
-
-
-function exibirTurma(){
-  //exibir turma na div 'resposta'
+    retorno.innerHTML = `O(A) estudante ${aluno.nomeDoAluno} foi adicionado(a) à turma ${aluno.anoClasse} ${aluno.turmaClasse}.`
 }
 
 function geraRA(){
+  //event listener
+  //this.ra.toString().padStart(7, "0"))
   return 2
 }
+
+function AddTurmas(){
+  const lista = document.getElementById('turmas-lista')
+  lista.innerHTML = ""
+  carregaAnoTurma() 
+}
+
+function addTurma(nomeTurma){
+  
+  const lista = document.getElementById('turmas-lista')
+  lista.innerHTML += `<option value="${nomeTurma}">${nomeTurma}</option>`
+}
+
+AddTurmas()
