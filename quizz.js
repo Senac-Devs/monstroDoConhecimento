2ff1528436031e05a8465260659e48dc13d38103
gutoDePaula <gutoramosdepaula@gmail.com>
2023-02-03 17:21:37 -0300

    Em progresso para exibir questões. #43

--- quizz.js
+++ quizz.js
@@ -3,7 +3,29 @@ bancoDados.transaction(function (exibe) {
         "SELECT * FROM perguntas",
         [],
         function (exibe, resultados) {
-            console.log(resultados);
+            exibePergunta(resultados.rows.item(1));
         }
     );
 });
+
+
+
+function exibePergunta(pergunta){
+    
+    const numeroQuestao = document.getElementById('numQuestao');
+    const enunciado = document.getElementById('pergunta');
+    const alternativaA = document.getElementById('a');
+    const alternativaB = document.getElementById('b');
+    const alternativaC = document.getElementById('c');
+    const alternativaD = document.getElementById('d');
+
+  // resultados.rows.item(i)
+    enunciado.innerHTML = pergunta.enunciado
+    alternativaA.innerHTML = pergunta.alternativaCorreta
+    alternativaB.innerHTML = pergunta.alternaticaErrada1
+    alternativaC.innerHTML = pergunta.alternaticaErrada2
+    alternativaD.innerHTML = pergunta.alternaticaErrada3
+    
+   //window.alert(pergunta.enunciado)
+    console.log(pergunta)
+}
