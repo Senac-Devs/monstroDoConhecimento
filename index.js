e083b4b14e0b117195fc73aade4d5cd89e6e26fe
gutoDePaula <gutoramosdepaula@gmail.com>
2023-02-06 12:04:48 -0300

    Prosseguindo com jogo de responder questionário. Falta concluir. #43

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-function testabotao(){
-    alert("botão funcionando");
-};
+function confereCookie (){
+    const cookies = document.cookie
+    if (cookies == ""){
+        carregaCookie()
+    }
+}
+
+function carregaCookie(){
+    document.cookie = JSON.stringify({'numeroQuestao': 0, 'listaQuestoes': []})
+}
+
+confereCookie()
+
+function pegaCookie (){
+    return JSON.parse(document.cookie)
+}
--- quizz.js
+++ quizz.js
@@ -3,29 +3,85 @@ bancoDados.transaction(function (exibe) {
         "SELECT * FROM perguntas",
         [],
         function (exibe, resultados) {
-            exibePergunta(resultados.rows.item(1));
+            const listaPerguntas = [];
+            for (let i = 0; i < resultados.rows.length; i++){
+                listaPerguntas.push(resultados.rows.item(i))
+            }
+            organizaPerguntas(listaPerguntas)
         }
     );
 });
 
+function organizaPerguntas(listaPerguntas){
+    const cookie = pegaCookie()
+    const perguntaAleatoria = listaPerguntas.sort(() => Math.random() - 0.5);
+    if (cookie.numeroQuestao == 0){
+        listaPerguntas.forEach((e)=> cookie["listaQuestoes"].push(e))
+    }
+    console.log(cookie)
+    exibePergunta(resultados.rows.item(perguntaAleatoria));
+}
 
+let idCorreto;
+let somCorreto = document.getElementById("som-correto");
+let somErrado = document.getElementById("som-errado");
 
-function exibePergunta(pergunta){
-    
-    const numeroQuestao = document.getElementById('numQuestao');
-    const enunciado = document.getElementById('pergunta');
-    const alternativaA = document.getElementById('a');
-    const alternativaB = document.getElementById('b');
-    const alternativaC = document.getElementById('c');
-    const alternativaD = document.getElementById('d');
+function tocarSomCerto() {
+    somCorreto.volume = 0.1;
+    somCorreto.play();
+}
 
-  // resultados.rows.item(i)
-    enunciado.innerHTML = pergunta.enunciado
-    alternativaA.innerHTML = pergunta.alternativaCorreta
-    alternativaB.innerHTML = pergunta.alternaticaErrada1
-    alternativaC.innerHTML = pergunta.alternaticaErrada2
-    alternativaD.innerHTML = pergunta.alternaticaErrada3
-    
-   //window.alert(pergunta.enunciado)
-    console.log(pergunta)
+function tocarSomErrado() {
+    somErrado.volume = 0.02;
+    somErrado.play();
+}
+
+let numPergunta = 1;
+function exibePergunta(pergunta) {
+    const nomeQuestionario = document.getElementById("titulo-questionario");
+    const numeroQuestao = document.getElementById("numQuestao");
+    const enunciado = document.getElementById("pergunta");
+    const alternativaA = document.getElementById("a");
+    const alternativaB = document.getElementById("b");
+    const alternativaC = document.getElementById("c");
+    const alternativaD = document.getElementById("d");
+
+    nomeQuestionario.innerText = pergunta.tema;
+    numeroQuestao.innerText = numPergunta; 
+    enunciado.innerText = pergunta.enunciado;
+    const listaAlternativas = [
+        pergunta.alternativaCorreta,
+        pergunta.alternaticaErrada1,
+        pergunta.alternaticaErrada2,
+        pergunta.alternaticaErrada3,
+    ];
+    listaAlternativas.sort(() => Math.random() - 0.5);
+    alternativaA.innerText = listaAlternativas[0];
+    alternativaB.innerText = listaAlternativas[1];
+    alternativaC.innerText = listaAlternativas[2];
+    alternativaD.innerText = listaAlternativas[3];
+    idCorreto = listaAlternativas.indexOf(pergunta.alternativaCorreta);
+    console.log(idCorreto);
+    console.log(pergunta);
+}
+let permissaoResponder = true;
+
+//POSSÍVEL PROBLEMA, o usuário poderia ficar apertando f5 para dar refresh
+//na página e refazer até acertar.
+function verificarSeAcertou(eleAlternativa, idAlternativa) {
+    if (permissaoResponder) {
+        if (idCorreto == idAlternativa) {
+            eleAlternativa.style.backgroundColor = "lightgreen";
+            tocarSomCerto();
+        } else {
+            eleAlternativa.style.backgroundColor = "lightcoral";
+            tocarSomErrado();
+        }
+        permissaoResponder = false;
+        numPergunta++;
+    }
+}
+
+function proximaQuestao(resultadorowsitem) {
+
 }
